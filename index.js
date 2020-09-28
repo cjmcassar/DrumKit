f9f7dcc06515b3f62bf95437b3a32489c5a578dc
Christopher-John Cassar <cjmcassar@users.noreply.github.com>
2020-09-28 23:40:34 +0100

    - Added button functionality to all of the buttons on the site

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 
-document.querySelector("button").addEventListener("click", handleClick);
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i<numberOfDrumButtons; i++)
+
+{
+
+document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
 
 function handleClick()
 {
-  alert("I got clicked!")
+  alert("I got clicked!");
+}
+
 }
