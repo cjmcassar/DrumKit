aca74ef9de24d255b2ee660e23b3cd71ffd9e66f
Christopher-John Cassar <cjmcassar@users.noreply.github.com>
2020-09-29 19:42:06 +0100

    - added sound functionality to js file - added background images to css

--- index.js
+++ index.js
@@ -2,14 +2,12 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i<numberOfDrumButtons; i++)
-
 {
-
 document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
-
 function handleClick()
 {
-  alert("I got clicked!");
+  var audio = new Audio("sounds/tom-1.mp3")
+  audio.play();
 }
 
 }
