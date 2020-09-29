ce513f3f34ba361f757b4a22f63e584468450b44
Christopher-John Cassar <cjmcassar@users.noreply.github.com>
2020-09-29 20:20:43 +0100

    - Added in this. test

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ for (var i = 0; i<numberOfDrumButtons; i++)
 document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
 function handleClick()
 {
-  var audio = new Audio("sounds/tom-1.mp3")
-  audio.play();
+  this.innerHTML
+  // var audio = new Audio("sounds/tom-1.mp3")
+  // audio.play();
 }
 
 }
